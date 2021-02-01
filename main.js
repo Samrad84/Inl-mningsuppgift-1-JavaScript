8fe3c3358b9e433f9207e956466794515a330ab2
Sam <samrad84@yahoo.com>
2021-02-01 23:55:08 +0100

    Add Script

--- main.js
+++ main.js
@@ -1,48 +1,96 @@
+
+// I det program so vi fick av Björn fanns det några buggar som jag försökte uptäcka
+// vi sätta Console.Log() där som behövdes. Jag har lagt till en knapp för att kunna 
+// spara ändringar på en enastående sida samr ett popup fönster om man väljer minus
+//sifra vid input. Jag ska utveckla vidare blogget och kan förklara mer vid behov,
+// det här vad en sammanfatning av vad jag har hunnit med.
+
 document.addEventListener("DOMContentLoaded", () => {
-    let inputBox = document.querySelector("#input-number");
-  
+  let inputBox = document.querySelector("#input-number");
+ 
+   
+
     inputBox.addEventListener("input", (event) => {
-      removeAllSections();
+    
       makeSections(parseInt(event.currentTarget.value));
+   
+     
     });
+  
   });
+ 
+ 
+ 
   
-  let makeSections = (count) => {
-    for (var i = 0; i < count; i++) {
-      var parent = document.querySelector("main");
+
+let makeSections = (count) => {
   
-      var child = document.createElement("section");
-      var title = document.createElement("h4");
-      var blurb = document.createElement("p");
+  if (count < 0 ) {
+    alert("This is an invalid number");
+  } else {
   
-      title.innerText = "Title " + i;
-      blurb.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-        Inventore harum, quasi dicta ex totam quisquam quo tempore maxime, 
-        commodi praesentium eius quod suscipit! 
-        Tenetur magnam eligendi amet fugiat adipisci impedit.`;
+ 
   
-      makeEditable(title);
-      makeEditable(blurb);
+    var parent = document.querySelector("main");
+  for (var i = 0; i < count; i++) {
+    var child = document.createElement("section");
+    var title = document.createElement("h4");
+    var blurb = document.createElement("p");
+    var saveButton = document.createElement("input");
+    
+    
+    
+    title.innerText = "Title " + i;
+    blurb.innerText = `Write Somethong here to save on your blog!`;
+    saveButton.type = 'submit';
+    saveButton.value = 'Save';
+    saveButton.onclick = "saveBlogg";
+   
+    
   
-      child.append(title);
-      child.append(blurb);
-      parent.append(child);
-    }
-  };
+
+    
+    console.log(i)
+  }
+ 
+
+  makeEditable(title);
+  makeEditable(blurb);
   
-  let removeAllSections = () => {
-    var test_sections = document.querySelectorAll("section");
-    for (var i = 0; i < test_sections.length; i++) {
-      test_sections[i].remove();
-    }
-  };
+
+  child.append(title);
+  child.append(blurb);
+  child.append(saveButton);
+  parent.append(child);
+ 
+  console.log(child) ;
+
+}
+};
+
+
+let removeLastSection = () => {
+  var test_sections = document.querySelectorAll("section");
   
-  let makeEditable = (elem) => {
-    elem.addEventListener("click", (event) => {
-      elem.contentEditable = true;
-      elem.focus();
-    });
-    elem.addEventListener("blur", (event) => {
-      elem.contentEditable = false;
-    });
-  };
+  for ( var i = 0; i < test_sections.length; i++) {
+   
+    test_sections[i].remove();
+  }
+};
+
+let makeEditable = (elem) => {
+  elem.addEventListener("click", (event) => {
+    elem.contentEditable = true;
+    elem.focus();
+    elem.saveBlogg();
+  });
+  elem.addEventListener("blur", (event) => {
+    elem.contentEditable = false;
+  });
+};
+function saveBlogg(){
+  var saveItem = document.getElementById("section").child;
+  localStorage.setItem("textvalue", saveItem);
+  console.log(saveItem);
+
+}
