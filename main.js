92f22223713e5dfb467bbf1a7f2fb949a0651439
Sam <samrad84@yahoo.com>
2021-02-14 09:31:16 +0100

    Add Comments

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// I det program so vi fick av Björn fanns det några buggar som jag försökte uptäcka
-// vi sätta Console.Log() där som behövdes. Jag har lagt till en knapp för att kunna
-// spara ändringar på en enastående sida samr ett popup fönster om man väljer minus
-//sifra vid input. Jag ska utveckla vidare blogget och kan förklara mer vid behov,
-// det här vad en sammanfatning av vad jag har hunnit med.
+// För att undvika buggar har jag försökt att inte använda globala variabler och funktioner har deklarerads som let vilket är det bästa för att undvika buggar.
+// Jag sätta Console.Log() där som behövdes för att se va är det för output och via dubugging satt ett
+// ett breack point för att undvika mistak i koden.
+//Jag har också använt "use strict" i min Js kod för att undvika errors.
 
+"use strict";
 document.addEventListener("DOMContentLoaded", () => {
   let inputBox = document.querySelector("#input-number");
 
@@ -16,11 +16,13 @@ let makeSections = (count) => {
   if (count < 0) {
     alert("This is an invalid number");
   } else {
-    var parent = document.querySelector("main");
+    let parent = document.querySelector("main");
     for (var i = 0; i < count; i++) {
       var child = document.createElement("section");
       var title = document.createElement("h4");
       var blurb = document.createElement("p");
+      //Här manipulerar jag DOM genom att skapa ett knapp (element) i main 
+      //dessutom ger jag dem egenskaper som behövs via JS för att ändra i HTML.
       var saveButton = document.createElement("input");
 
       title.innerText = "Title " + i;
@@ -51,11 +53,13 @@ let makeEditable = (elem) => {
   elem.addEventListener("click", (event) => {
     elem.contentEditable = true;
     elem.focus();
-   
   });
   elem.addEventListener("blur", (event) => {
+    // Här försöker jag minipulera DOM via ett eventhandler då 
+    //varje gång användare skriver något läggs till texten i Localstorage och
+    // med knappen sparar man den nya outputen i en ny siad.
+    let saveButton = document.querySelector("input");
     elem.contentEditable = false;
-    var saveButton = document.createElement("input");
     saveButton.onclick = saveBlogg(elem);
 
     console.log(elem.innerText);
